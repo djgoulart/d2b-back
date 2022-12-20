caf2b98c9d583848c5433917661d368458f9ac4a
Diego Goulart <diegogoulart.aws@gmail.com>
2022-12-20 14:43:30 -0300

    feat: create relationship btw account and transaction

--- app/Models/Account.php
+++ app/Models/Account.php
@@ -39,4 +39,9 @@ class Account extends Model
     {
         return $this->belongsTo(User::class);
     }
+
+    public function transactions()
+    {
+        return $this->hasMany(Transaction::class);
+    }
 }
