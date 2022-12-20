4176727418b55817c7f0d34f854f223cb3ab3098
Diego Goulart <diegogoulart.aws@gmail.com>
2022-12-19 23:35:43 -0300

    test: tests for increment an account balance

---
+++ tests/Unit/D2b/Application/Customer/Account/IncrementBalanceUseCaseTest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Tests\Unit\D2b\Application\Customer\Account;
+
+use D2b\Application\Dto\Customer\Account\IncrementBalanceInputDto;
+use D2b\Application\Dto\Customer\Account\IncrementBalanceOutputDto;
+use D2b\Application\UseCase\Customer\Account\IncrementBalanceUseCase;
+use D2b\Domain\Customer\Entities\User;
+use D2b\Domain\Customer\Entities\Account;
+use D2b\Domain\Customer\Repositories\AccountRepositoryInterface;
+use Mockery;
+use Ramsey\Uuid\Uuid;
+use stdClass;
+use Tests\TestCase;
+
+class IncrementBalanceUseCaseTest extends TestCase
+{
+    public function test_it_should_increment_an_account_balance()
+    {
+        $userId = Uuid::uuid4()->toString();
+        $name = 'User Test';
+        $email = 'user@test.com';
+        $password = 'user_password';
+        $roleId = 10;
+
+        $id = Uuid::uuid4()->toString();
+        $owner = $userId;
+
+        $value = 100;
+
+        $this->mockUser = Mockery::mock(User::class, [
+            $password, $roleId, $userId, $email, $name
+        ]);
+
+        $this->mockEntity = Mockery::mock(Account::class, [
+            $owner, $id,
+        ]);
+
+        $this->mockInputDto = Mockery::mock(IncrementBalanceInputDto::class, [
+            $this->mockEntity,
+            $value
+        ]);
+
+        $this->mockEntity->shouldReceive('createdAt')->andReturn(date('Y-m-d H:i:s'));
+
+        $this->mockRepo = Mockery::mock(stdClass::class, AccountRepositoryInterface::class);
+        $this->mockRepo->shouldReceive('incrementBalance')->times(1)->andReturn($this->mockEntity);
+
+        $useCase = new IncrementBalanceUseCase($this->mockRepo);
+        $result = $useCase->execute($this->mockInputDto);
+
+        $this->assertInstanceOf(IncrementBalanceOutputDto::class, $result);
+
+        Mockery::close();
+    }
+}
